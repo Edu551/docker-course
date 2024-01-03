4d23c82010e8ebd31e2318ac3b0484c4259d053e
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2024-01-03 07:14:26 -0300

    change test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>hello world!!!</p>
                 <p>Texto novo</p>
+                <p>New change!!!!!</p>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
